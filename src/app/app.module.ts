454489b606c44f81282e281b6536b54e65ea3a0b
Ntonsite Mwamlima <nmwamlima@accessbank.internal>
2020-10-27 11:27:17 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product/product.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import {ProductService} from './shared/product.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/product.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {Product} from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  formData: Product;
+  constructor() { }
+}
